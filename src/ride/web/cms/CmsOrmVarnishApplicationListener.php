a7a0a80b7216a43ff4061b4f82b46638f2f10bd5
Kaya <kayalion@kayalion.net>
2015-11-19 14:21:48 +0100

    initial commit

---
+++ src/ride/web/cms/CmsOrmVarnishApplicationListener.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace ride\web\cms;
+
+use ride\library\event\Event;
+use ride\library\event\EventManager;
+use ride\library\i18n\I18n;
+use ride\library\orm\model\GenericModel;
+
+use ride\web\cms\orm\varnish\OrmVarnishInfo;
+use ride\web\WebApplication;
+
+use ride\service\CmsVarnishService;
+
+/**
+ * Application listener to handle varnish for CMS ORM integration
+ */
+class CmsOrmVarnishApplicationListener {
+
+    /**
+     * Constructs a new instance
+     * @param \ride\service\CmsVarnishService $cmsVarnishService
+     * @param \ride\web\cms\orm\varnish\OrmVarnishInfo $ormVarnishInfo
+     * @param \ride\library\event\EventManager $eventManager
+     * @param \ride\library\i18n\I18n
+     * @param string $baseUrl
+     * @return null
+     */
+    public function __construct(CmsVarnishService $cmsVarnishService, OrmVarnishInfo $ormVarnishInfo, EventManager $eventManager, I18n $i18n, $baseUrl) {
+        $this->varnishService = $cmsVarnishService;
+        $this->varnishInfo = $ormVarnishInfo;
+        $this->eventManager = $eventManager;
+        $this->i18n = $i18n;
+        $this->baseUrl = $baseUrl;
+        $this->banUrls = array();
+    }
+
+    /**
+     * Handles a ORM action to detect needed varnish bans
+     * @param \ride\library\event\Event $event
+     * @return null
+     */
+    public function handleOrmAction(Event $event) {
+        $model = $event->getArgument('model');
+        $entry = $event->getArgument('entry');
+
+        switch ($event->getName()) {
+            case GenericModel::EVENT_UPDATE_PRE:
+            case GenericModel::EVENT_DELETE_PRE:
+                // gather detail urls
+                $this->gatherDetailUrls($model, $entry);
+
+                $this->addBanListener();
+
+                break;
+            case GenericModel::EVENT_INSERT_POST:
+            case GenericModel::EVENT_UPDATE_POST:
+            case GenericModel::EVENT_DELETE_POST:
+                // gather overview urls
+                $this->gatherOverviewUrls($model, $entry);
+
+                $this->addBanListener();
+
+                break;
+        }
+    }
+
+    /**
+     * Adds the event listener to perform the bans
+     * @return null
+     */
+    private function addBanListener() {
+        if (!$this->banUrls || isset($this->isBanEventRegistered)) {
+            return;
+        }
+
+        $this->eventManager->addEventListener(WebApplication::EVENT_PRE_RESPONSE, array($this, 'handleBan'));
+
+        $this->isBanEventRegistered = true;
+    }
+
+    /**
+     * Handles the actual ban event
+     * @param \ride\library\event\Event $event
+     * @return null
+     */
+    public function handleBan(Event $event) {
+        $this->performBan();
+    }
+
+    /**
+     * Performs a ban command with all gathered URL's
+     * @return null
+     */
+    private function performBan() {
+        foreach ($this->banUrls as $url => $recursive) {
+            $this->varnishService->banUrl($url, $recursive);
+        }
+    }
+
+    /**
+     * Gathers the detail URL's for the provided entry
+     * @param \ride\library\orm\model\GenericModel $model
+     * @param \ride\library\orm\entry\Entry $entry
+     * @return null
+     */
+    private function gatherDetailUrls(GenericModel $model, $entry) {
+        $locale = $this->getLocale($entry);
+
+        $this->banUrls += $this->varnishInfo->getDetailUrls($model->getName(), $entry, $locale, $this->baseUrl);
+    }
+
+    /**
+     * Gathers the overview URL's for the provided entry
+     * @param \ride\library\orm\model\GenericModel $model
+     * @param \ride\library\orm\entry\Entry $entry
+     * @return null
+     */
+    private function gatherOverviewUrls(GenericModel $model, $entry) {
+        $locale = $this->getLocale($entry);
+
+        $this->banUrls += $this->varnishInfo->getOverviewUrls($model->getName(), $locale, $this->baseUrl);
+    }
+
+    /**
+     * Gets the locale of the entry
+     * @param \ride\library\orm\entry\Entry $entry
+     * @return string
+     */
+    private function getLocale($entry) {
+        if ($entry instanceof LocalizedEntry) {
+            return $entry->getLocale();
+        }
+
+        return $this->i18n->getLocale()->getCode();
+    }
+
+}
